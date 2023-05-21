6d2dff7d61fc3effa9f22c84b9ef3164f19c3a8a
Emilio Bensignor <liobensignor@gmail.com>
2023-05-20 22:29:33 -0300

    Todo menos video - prueba Mobile

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// VERIFICACIÓN
+
+function verificarRespuesta() {
+    var respuesta = document.getElementById("respuesta").value;
+
+    if (respuesta.toLowerCase() === "olivia") {
+        // Redirigir al usuario a la siguiente sección
+        window.location.href = "pages/home.html";
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: '¿No la sabes?',
+            text: 'A seguir intentando...',
+        });
+    }
+}
+
+// NAVEGACIÓN
+
+function navigateToPage(pageName) {
+    window.location.href = pageName + '.html';
+}
+
+// CONTENIDO
+
+function mostrarContenido(id) {
+    // Ocultar todos los contenidos
+    let contenidos = document.getElementsByClassName('contenido');
+    for (let i = 0; i < contenidos.length; i++) {
+        contenidos[i].style.display = 'none';
+    }
+
+    // Mostrar el contenido seleccionado
+    let contenido = document.getElementById(id);
+    contenido.style.display = 'block';
+}
