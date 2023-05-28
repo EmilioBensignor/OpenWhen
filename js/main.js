d947a90a1bc0650f8ea13e76f6b9276ded5a2d49
Emilio Bensignor <liobensignor@gmail.com>
2023-05-27 21:10:31 -0300

    Dia 28

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 function verificarRespuesta() {
     var respuesta = document.getElementById("respuesta").value;
 
-    if (respuesta.toLowerCase() === "brocoli") { // La respuesta en minúscula
+    if (respuesta.toLowerCase() === "superejercicio") { // La respuesta en minúscula
         // Redirigir al usuario a la siguiente sección
         window.location.href = "pages/home.html";
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Pista',
-            text: 'Es verdeee',
+            text: 'Parecido a los superalimentos',
         });
     }
 }
