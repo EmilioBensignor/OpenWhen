f57aa2a24b401ff0303f3f5cc246d7910c905fb5
Emilio Bensignor <liobensignor@gmail.com>
2023-06-04 19:58:04 -0300

    Dia 5

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@
 function verificarRespuesta() {
     var respuesta = document.getElementById("respuesta").value;
 
-    if (respuesta.toLowerCase() === "anakin") { // La respuesta en minúscula
+    if (respuesta.toLowerCase() === "azul") { // La respuesta en minúscula
         // Redirigir al usuario a la siguiente sección
         window.location.href = "pages/home.html";
     } else {
         Swal.fire({
             icon: 'error',
             title: 'Pista',
-            text: 'Amigo de Obi Wan',
+            text: 'Daa esta la sabes',
         });
     }
 }
